5a09f94beb12b43a3797f4f548844761c47d72bf
Iker Gonzalez Tirado <52651951+LinkerG@users.noreply.github.com>
2024-06-11 13:06:10 +0200

    tailwind

--- webpack.config.ts
+++ webpack.config.ts
@@ -27,7 +27,11 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [
+          'style-loader',
+          'css-loader',
+          'postcss-loader'
+        ],
       },
     ],
   },
